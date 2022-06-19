ee669aa6e71344894b7d575ab7892ade81c34fbd
fabiotbraga <fabiotbraga@gmail.com>
2022-06-19 18:13:22 -0300

    bootcamp tqi

---
+++ Curso_js/teste.js
@@ -0,0 +1,40 @@
+function soma (a, b) {
+    return a + b;
+}
+
+function sub (a, b) {
+    return a - b;
+}
+
+function mult (a, b) {
+    return a * b;
+}
+
+function div (a, b) {
+    return a / b;
+}
+
+function tela_inicio (){
+    /* def calculadora():
+    option = int(input('Digite sua opção (1/2/3/4): '))
+    num1 = int(input('Primeiro número: '))
+    num2 = int(input('Segundo número: '))
+    
+    if option == 1:
+        soma(num1,num2)
+              
+    elif option == 2:
+        sub(num1,num2)
+            
+    elif option == 3:
+        mult(num1,num2)
+            
+    elif option == 4:
+        divi(num1,num2)
+               
+    else:
+        print('\nOpção Inválida!!') */
+    
+
+
+}
